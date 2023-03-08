33a1d2aad9fa2d74d4aff22878d3bc68f97dce38
Michael Hendrick <mhendrick@byui.edu>
2023-03-07 22:45:18 -0700

    Drop down descriptions too!

--- src/Home.js
+++ src/Home.js
@@ -32,7 +32,8 @@ function Home() {
 function Grades() {
   const {isLoading, error, data} = useQuery('Grades', fetchData);
   const grades = data ?? []
-  const [shownCourseAssignments, setShownCourseAssignments] = useState([false]);
+  const [shownCourseAssignments, setShownCourseAssignments] = useState([]);
+  const [shownAssignmentDescriptions, setShownAssignmentDescriptions] = useState([]);
 
   if(isLoading){
     return <div className='Loading'>Loading...</div>;
@@ -46,29 +47,47 @@ function Grades() {
       <div className='Home'>
         <div className='Grade-Grid'>
           <div className='Grade-Column-Headers'>
-            <div className='Course-Name'>Course Name</div>
-            <div className='Grade'>Grade</div>
-            <div className='Course-ID'>Course ID</div>
+            <div className='Header Name'>Name</div>
+            <div className='Header Grade'>Grade</div>
+            <div className='Header ID'>ID</div>
           </div>
-          {grades.map((gradeObject, index) => 
-            <div key={gradeObject?.course_id} onClick={() => {
-              // Right now this is re-rendering the entire page, and I don't really know why or how to make it better
+          {grades.map((gradeObject, i) => 
+            <div key={gradeObject?.course_id}>
+              <div className='Course Grade-Row' onClick={() => {
               var shownAssignments = [...shownCourseAssignments];
-              shownAssignments[index] = !shownAssignments[index];
+              shownAssignments[i] = !shownAssignments[i];
               setShownCourseAssignments(shownAssignments);
             }}>
-              <div className='Course Grade-Row'>
-                <div className='Course-Name'> {gradeObject?.course_name} </div>
-                <div className='Course-Grade'> {gradeObject?.grade_percentage}% ({gradeObject?.grade_letter})</div>
-                <div className='Course-ID'> {gradeObject?.course_id} </div>
+                <div className='Course Name'> {gradeObject?.course_name} </div>
+                <div className='Course Grade'> {gradeObject?.grade_percentage}% ({gradeObject?.grade_letter})</div>
+                <div className='Course ID'> {gradeObject?.course_id} </div>
               </div>
-            {shownCourseAssignments[index] ? gradeObject?.assignments.map((assignmentObject) => 
-              <div  key={assignmentObject?.id} className={'Assignment Grade-Row'}>
-                <div className='Assignment-Name'> {assignmentObject?.name} </div>
-                <div className='Assignment-Name' dangerouslySetInnerHTML={{__html: assignmentObject?.description}}></div>
-                <div className='Assignment-Grade'> {assignmentObject?.grade_percentage}% ({assignmentObject?.grade_letter})</div>
-                <div className='Assignment-ID'> {assignmentObject?.id} </div>
-              </div>
+            {shownCourseAssignments[i] ? gradeObject?.assignments.map((assignmentObject, j) => 
+              <>
+                <div  key={assignmentObject?.id} className={'Assignment Grade-Row'} onClick={() => {
+              var shownDescriptions = [...shownAssignmentDescriptions];
+              if (!shownDescriptions[i]) {
+                shownDescriptions[i] = [];
+                shownDescriptions[i][j] = true;
+              }
+              else shownDescriptions[i][j] = !shownDescriptions[i][j];
+              setShownAssignmentDescriptions(shownDescriptions);
+            }}>
+                  <div className='Assignment Name'> -- {assignmentObject?.name} </div>
+                  <div className='Assignment Grade'> -- {
+                    assignmentObject?.workflow_state === 'submitted' ? 
+                    'Not Yet Graded':
+                    assignmentObject?.grade_percentage === null ?
+                    'Doesn\'t count towards final grade':
+                    assignmentObject?.workflow_state === 'graded' ?
+                    assignmentObject?.grade_percentage + '% (' + assignmentObject?.grade_letter + ')'
+                    : 
+                    'Not Turned In'
+                  }</div>
+                  <div className='Assignment ID'> -- {assignmentObject?.id} </div>
+                </div>
+                {shownAssignmentDescriptions[i] ? shownAssignmentDescriptions[i][j] ? <div className='Assignment Description' dangerouslySetInnerHTML={{__html: assignmentObject?.description}}></div>: '': ''}
+              </>
             ): ''}
             </div>
           )}
